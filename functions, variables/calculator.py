dc423ee3b561c554f1e425dfe55e11e50c375e17
LoveCode1389 <erfankhajezahde@gmail.com>
2023-09-23 17:10:56 +0330

    Python

---
+++ functions, variables/calculator.py
@@ -0,0 +1,6 @@
+x = float(input("What's is x? "))
+y = float(input("What's is y? "))
+
+z = x / y
+
+print(f"{z:.2f}")
---
+++ functions, variables/hello.py
@@ -0,0 +1,5 @@
+# Ask user for their name
+name = input("What's your name? ").strip().title()
+
+# Say hello to user
+print(f"hello, {name}")
