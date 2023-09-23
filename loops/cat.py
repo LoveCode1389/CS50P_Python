e9d396b8228f8b7ad0a1cc59af2f56fe3ec2e060
LoveCode1389 <erfankhajezahde@gmail.com>
2023-09-23 18:01:59 +0330

    Python

--- loops/cat.py
+++ loops/cat.py
@@ -1,1 +1,17 @@
-print("meow" * 3)
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+    return n
+
+def meow(n):
+    print('')
+    for _ in range(n):
+        print("meow")
+
+main()
