a4be6f6efcdf7081e032383fdc76b4b3fba51305
LoveCode1389 <erfankhajezahde@gmail.com>
2023-09-23 18:55:28 +0330

    Python

--- loops/hogwarts.py
+++ loops/hogwarts.py
@@ -1,4 +1,2 @@
-students = ['Hermione', 'Harry', 'Ron']
-
-for i in range(len(students)):
-    print(i + 1, students[i])
+students = ['Hermione', 'Harry', 'Ron', 'Draco']
+houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
