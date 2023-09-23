31c06d7d7b6df2c50f73bbbb5edb6ca4611176f0
LoveCode1389 <erfankhajezahde@gmail.com>
2023-09-23 20:19:59 +0330

    Python

--- loops/hogwarts.py
+++ loops/hogwarts.py
@@ -1,1 +1,6 @@
-students = []
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
