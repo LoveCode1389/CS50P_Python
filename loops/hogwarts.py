9999a82223807f3080c8b6b8241f29692cf95409
LoveCode1389 <erfankhajezahde@gmail.com>
2023-09-23 19:22:29 +0330

    Python

--- loops/hogwarts.py
+++ loops/hogwarts.py
@@ -1,2 +1,18 @@
-students = ['Hermione', 'Harry', 'Ron', 'Draco']
-houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": 'Gryffindor',
+    "Draco": "Slytherin",
+}
+
+# my work(for design)
+one = 0
+n = []
+
+for i in students:
+    n.append(len(i))
+one = max(n)
+
+for student in students:
+    faseleh = one - len(student)
+    print(student + (' ' * faseleh), "|" ,students[student])
