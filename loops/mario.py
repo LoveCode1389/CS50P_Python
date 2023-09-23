f6e8f5541a6330ea8bc242ee1f41a96a7675fcc7
LoveCode1389 <erfankhajezahde@gmail.com>
2023-09-23 21:58:21 +0330

    Python

--- loops/mario.py
+++ loops/mario.py
@@ -22,10 +22,17 @@ def square():
     def main():
         print_square(3)
 
-
     def print_square(size):
+
+        # For each row in square
         for i in range(size):
-            
+
+            # For each brick in row
+            for j in range(size):
+
+                # Print Brick
+                print("#", end="")
+            print()
 
     main()
 
