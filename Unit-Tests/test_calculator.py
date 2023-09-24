cbe9ed5db43d666be477ff1185355c10082f08fc
LoveCode1389 <erfankhajezahde@gmail.com>
2023-09-24 18:30:02 +0330

    Python

--- Unit-Tests/test_calculator.py
+++ Unit-Tests/test_calculator.py
@@ -1,3 +1,4 @@
+import pytest
 from calculator import square
 
 def test_square():
@@ -6,3 +7,5 @@ def test_square():
     assert square(-2) == 4
     assert square(-3) == 9
     assert square(0) == 0
+    with pytest.raises(TypeError):
+        square("cat")
