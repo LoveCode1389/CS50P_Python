b3fbfaff6899117f7d7a6d788f0d4fb1e9abf20a
LoveCode1389 <erfankhajezahde@gmail.com>
2023-09-24 18:18:13 +0330

    Python

--- Unit-Tests/my_test_calculator.py
+++ Unit-Tests/my_test_calculator.py
@@ -5,7 +5,10 @@ from calculator import square
 def main():
     test_square()
 
+
 def test_square():
+    correct = 0
+
     for arg in range(20):
         arg += 1
         try:
@@ -14,6 +17,10 @@ def test_square():
             print(f'{arg} squared, was not {arg * arg}')
         else:
             print(f'{arg} squared, is {arg * arg}')
+            correct += 1
+    for j in range(5):
+        print()
+    print(f"This function answered {correct} out of 20 questions correctly")
 
 
 if __name__ == "__main__":
