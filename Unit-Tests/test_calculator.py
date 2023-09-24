1f07fc5d2636d6286542146a52ee52e27e219c06
LoveCode1389 <erfankhajezahde@gmail.com>
2023-09-24 17:43:33 +0330

    Python

--- Unit-Tests/test_calculator.py
+++ Unit-Tests/test_calculator.py
@@ -1,20 +1,8 @@
 from calculator import square
 
-# I modified the CS50P code for better output
-
-def main():
-    test_square()
-
 def test_square():
-    for arg in range(20):
-        arg += 1
-        try:
-            assert square(arg) == arg * arg
-        except AssertionError:
-            print(f'{arg} squared, was not {arg * arg}')
-        else:
-            print(f'{arg} squared, is {arg * arg}')
-
-
-if __name__ == "__main__":
-    main()
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
